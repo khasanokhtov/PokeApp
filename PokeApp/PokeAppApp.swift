7ceac8b89b32300045e68cbce9557c4b5cbef36f
khasanokhtov <76727992+khasanokhtov@users.noreply.github.com>
2023-05-14 10:14:43 +0300

    Initial Commit

---
+++ PokeApp/PokeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeAppApp.swift
+//  PokeApp
+//
+//  Created by Alex Okhtov on 14.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct PokeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
